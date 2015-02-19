21e62fae3371ef48f351569c72be88863830d480
tatsy <tatsy.mail@gmail.com>
2015-02-19 17:01:29 +0900

    Fix packaging for bower. Now the package for bower is generated with WebPack.

--- Makefile
+++ Makefile
@@ -1,15 +1,19 @@
 coverage:
-			rm -rf coverage
-			istanbul cover node_modules/.bin/_mocha
+	rm -rf coverage
+	istanbul cover node_modules/.bin/_mocha
 
 lint:
-				eslint --reset .
+	eslint --reset .
+
+publish:
+	webpack ./
+	uglifyjs dist/markdown-it-imsize.js > dist/markdown-it-imsize.min.js
 
 test: lint
-			mocha
+	mocha
 
 test-ci: lint
-			istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
+	istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
 
 .PHONY: lint coverage
 .SILENT: lint
--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var assert = require('assert');
 var path = require('path');
 var generate = require('markdown-it-testgen');
 
@@ -8,7 +9,7 @@ describe('markdown-it-imsize', function() {
     html: true,
     linkify: true,
     typography: true
-  }).use(require('../'));
+  }).use(require('../lib'));
   generate(path.join(__dirname, 'fixtures/markdown-it-imsize/imsize.txt'), md);
 });
 
@@ -17,6 +18,19 @@ describe('markdown-it-imsize (autofill)', function() {
     html: true,
     linkify: true,
     typography: true
-  }).use(require('../'), { autofill: true });
+  }).use(require('../lib'), { autofill: true });
   generate(path.join(__dirname, 'fixtures/markdown-it-imsize/autofill.txt'), md);
+});
+
+describe('image size detector', function() {
+  it('image size detector', function(done) {
+    var imsize = require('../lib/imsize');
+    var types = require('../lib/imsize/types');
+    types.forEach(function(type) {
+      var dim = imsize('./test/img/lena.' + type);
+      assert.equal(dim.width, 128);
+      assert.equal(dim.height, 128);
+    });
+    done();
+  });
 });
