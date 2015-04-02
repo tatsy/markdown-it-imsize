d84509903630e221aecde51a9191957dd1b9a6b2
tatsy <tatsy.mail@gmail.com>
2015-04-02 19:13:49 +0900

    Support markdown-it 4.0.0

--- test/commonmark.js
+++ test/commonmark.js
@@ -1,13 +1,33 @@
 'use strict';
 
 
-var path = require('path');
+var p      = require('path');
+var load   = require('markdown-it-testgen').load;
+var assert = require('chai').assert;
+
+function normalize(text) {
+  return text.replace(/<blockquote>\n<\/blockquote>/g, '<blockquote></blockquote>');
+}
 
 
-var generate = require('markdown-it-testgen');
+function generate(path, md) {
+  load(path, function (data) {
+    data.meta = data.meta || {};
+
+    var desc = data.meta.desc || p.relative(path, data.file);
+
+    (data.meta.skip ? describe.skip : describe)(desc, function () {
+      data.fixtures.forEach(function (fixture) {
+        it(fixture.header ? fixture.header : 'line ' + (fixture.first.range[0] - 1), function () {
+          assert.strictEqual(md.render(fixture.first.text), normalize(fixture.second.text));
+        });
+      });
+    });
+  });
+}
 
 describe('CommonMark', function () {
   var md = require('markdown-it')('commonmark').use(require('../'));
 
-  generate(path.join(__dirname, 'fixtures/commonmark/good.txt'), md);
+  generate(p.join(__dirname, 'fixtures/commonmark/good.txt'), md);
 });
